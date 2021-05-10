64e51f872b82f9efc2236be889927a909a642e3a
Rinooj NR <rinoojnr@gmail.com>
2021-05-10 19:55:24 +0530

    added uc5 branch

---
+++ calculating_wages_for_a_month.sh
@@ -0,0 +1,32 @@
+for((i=1;i<=20;i++))
+do
+is_part_time=1
+is_full_time=0
+emp_rate_per_hr=20
+emp_check=$(($RANDOM%3))
+case $emp_check in
+$is_full_time)
+emphrs=8
+;;
+$is_part_time)
+emphrs=4
+;;
+*)
+emphrs=0
+;;
+esac
+echo $i " Employee hours is :"$emphrs
+salary=$(($emphrs * $emp_rate_per_hr));
+echo " Salary ot the day= "$salary
+done
+array=($salary)
+echo "array"$array
+sum=0
+
+for i in ${array[@]}
+do
+    sum=`expr $sum + $i`
+done
+
+echo "Total salary is ="$sum
+
